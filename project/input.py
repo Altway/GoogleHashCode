2d010f75c58b577c6fc1d23bc5d6b9b0ea82722d
Robin Seichais <r.seichais@criteo.com>
2018-02-25 00:39:10 +0100

    First attemps at solving this thing

--- project/input.py
+++ project/input.py
@@ -3,17 +3,9 @@ import sys
 from collections import namedtuple
 
 
-FILES = {
-    "s": 'data/small.in',
-    "small": 'data/small.in',
-    "m": 'data/medium.in',
-    "medium": 'data/small.in',
-    "b": 'data/big.in',
-    "big": 'data/big.in',
-}
-
 TOMATO = 'T'
 MUSHROOM = 'M'
+
 
 Problem = namedtuple('Problem', 'height width min_ingredient max_size data')
 
@@ -26,19 +18,11 @@ def read_data_file(path):
         data = [list(l[:-1]) for l in lines[1:]]
 
         problem = Problem(
-            width=width,
-            height=height,
-            min_ingredient=min_ingredient,
-            max_size=max_size,
+            width=int(width),
+            height=int(height),
+            min_ingredient=int(min_ingredient),
+            max_size=int(max_size),
             data=data
         )
 
     return problem
-
-
-if __name__ == '__main__':
-    assert len(sys.argv) == 2
-    data_file = FILES[sys.argv[1]]
-    pb = read_data_file(data_file)
-
-    print(pb)
